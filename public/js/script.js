44d31d3100781112d57b6d70171832cad709e856
Sarah-Masih <70310294+Sarah-Masih@users.noreply.github.com>
2023-05-18 23:17:44 +1000

    Sockets added to server, script and index files

--- public/js/script.js
+++ public/js/script.js
@@ -38,6 +38,12 @@ const addProjectToApp = (project) => {
     });
 }
 
+let socket = io();
+socket.on('number', (message)=> {
+    console.log('received from server' + message);
+});
+
+
 $(document).ready(function(){
     $('.materialboxed').materialbox();
     $('.modal').modal();
--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 var express = require('express');
 var app = express();
 require('./dbConnection');
-
 let router = require('./route/route');
-
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/',router);
 
+io.on('connection', (socket) => {
+    console.log('connected a client');
+    socket.on('disocnnect', ()=>{
+        console.log('client has disconnected')
+    });
+    setInterval(()=>{
+        socket.emit('number' + parseInt(Math.random()*10));
+    }, 1000);
+});
 
-//app.listen(port,()=>{
-    //console.log('App listening to: ' + port);
-//});
-
-//app.get('/api/cats', (req,res) => {
- //   res.json({statusCode: 200, data: cardList, message: 'Success'});
-//})
 
 var port = process.env.port || 3000;
-app.listen(port, () => {
+http.listen(port, () => {
     console.log('App listening to: ' + port);
-    //dbConnection('Cats');
 });
